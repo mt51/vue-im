bf8c465dffa0df0c88d7774828f22b7f2ec0fcc6
liuziyang <798230885@qq.com>
2017-12-27 23:25:44 +0800

    添加全局提醒组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import IM from './im'
+import Message from './components/message.js'
 
 const VueIM = {}
 
 VueIM.install = function (Vue, opts) {
   Vue.component('vue-im', IM)
+  Vue.prototype.$message = Message
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
