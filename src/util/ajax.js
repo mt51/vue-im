2e1481243bc69e73ca123fa73331b819f707ffb4
liuziyang <ziyang.liu@roadshare.com>
2018-03-06 16:38:33 +0800

    内置ajax请求添加get方式

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -1,4 +1,4 @@
-export default function ajax (options) {
+export function post (options) {
   if (!XMLHttpRequest) return
   const xhr = new XMLHttpRequest()
   const formData = new FormData()
@@ -9,6 +9,23 @@ export default function ajax (options) {
 
   xhr.send(formData)
 
+  xhr.onreadystatechange = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText)
+        options.onSuccess(data)
+      } else {
+        options.onError(xhr)
+      }
+    }
+  }
+}
+
+export function get (options) {
+  const xhr = new XMLHttpRequest()
+  const { url, data } = options
+  xhr.open('GET', url)
+  xhr.send(data)
   xhr.onreadystatechange = function (e) {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
